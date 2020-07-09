9e47a3f0baa7b0ba53580f1795613c7121843f90
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-09 20:25:31 -0300

    Creating templates and macros

--- keysfood/ext/site/main.py
+++ keysfood/ext/site/main.py
@@ -6,4 +6,12 @@ bp = Blueprint("site", __name__)
 
 @bp.route("/")
 def index():
-    return render_template("index.html", name=request.args["name"])
+    return render_template("index.html")
+
+@bp.route("/about")
+def about():
+    return render_template("about.html")
+
+@bp.route("/restaurants")
+def restaurants():
+    return render_template("restaurants.html")
