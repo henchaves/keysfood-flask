489d1c6d7b5b456bad6c84de222e5bb36f30aa20
Henrique Chaves <henriquechavesmm@gmail.com>
2020-07-11 23:13:24 -0300

    Added cli

--- keysfood/ext/cli/__init__.py
+++ keysfood/ext/cli/__init__.py
@@ -1,6 +1,7 @@
 import click
 from keysfood.ext.db import db
-from keysfood.ext.site import models # noqa
+from keysfood.ext.site import models
+
 
 def init_app(app):
 
@@ -8,6 +9,21 @@ def init_app(app):
     def create_db():
         """This command initializes a db"""
         db.create_all()
+
+    @app.cli.command()
+    @click.option("--email", "-e")
+    @click.option("--passwd", "-p")
+    @click.option("--admin", "-a", is_flag=True, default=False)
+    def add_user(email, passwd, admin):
+        """Add a new user"""
+        user = models.User(
+            email=email,
+            passwd=passwd,
+            admin=admin
+        )
+        db.session.add(user)
+        db.session.commit()
+        click.echo(f"Usuário {email} criado com suecesso!")
 
     @app.cli.command()
     def listar_pedidos():
